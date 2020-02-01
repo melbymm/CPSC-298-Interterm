33507ce86b249470d6b121442a2b833a92494d8f
Mitch <melby@chapman.edu>
2020-02-01 01:31:11 +0000

    HI

--- Assignment1/Assignment1Problem4.cpp
+++ Assignment1/Assignment1Problem4.cpp
@@ -2,21 +2,22 @@
 using namespace std;
 
 main(int argc, char **argv){
+//initialize variables
   float cost = 0.00;
   float inflationRate = 0.00;
   float inflationDec = 0.00;
   float years = 0.00;
   float futureCost = 0.00;
-
+//prompt user input
   cout << "How much does the item cost?: " << endl;
   cin >> cost;
   cout << "What is the inflation rate?(Percent): " << endl;
   cin >> inflationRate;
   cout << "How many years in the future?: " << endl;
   cin >> years;
-
+//convert inflation percent to decimal
   inflationDec = inflationRate/100;
-
+//calculate cost in future
   futureCost = cost + (cost * inflationDec * years);
 
   cout << "Your adjusted budget is: " << futureCost << endl;
